3c59d9f97264bbeda6e642799e5f112ee077964e
Collin Jia <collinjia16@gmail.com>
2025-10-01 18:36:54 -0400

    update counter

--- src/counter.v
+++ src/counter.v
@@ -1,18 +1,23 @@
 module tt_um_ccjiaa_counter (
-    input wire [7:0]  uio,         // for clock and reset
-    output wire [7:0]  uo         // 8-bit output count value
+    input wire [7:0]  uio,         // for load value
+    input wire [7:0] ui,            // for clock, reset, enable
+    output wire [7:0] uo         // 8-bit output count value
 );
 
     wire clk;
     wire rst_n;
+    wire enable;
     reg [7:0] count;
 
-    assign clk   = uio[0];
-    assign rst_n = uio[1];
+    assign clk   = ui[0];
+    assign rst_n = ui[1];
+    assign enable = ui[2];
 
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n)
             count <= 8'd0;
+        else if (uio)
+            count <= uio
         else
             count <= count + 1;
     end
